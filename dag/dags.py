15156a482f4430542c710eea348386a6e25409eb
v-smit <smit.vaghasiya@rackspace.com>
2024-02-16 13:18:37 +0530

    dag changed

--- dag/dags.py
+++ dag/dags.py
@@ -7,6 +7,7 @@ from cosmos import DbtDag, LoadMode, RenderConfig, DbtTaskGroup, ProfileConfig, 
 from cosmos.profiles import DatabricksTokenProfileMapping
 from cosmos.constants import TestBehavior
 from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator
+from airflow.operators.bash_operator import BashOperator
 
 PROJECT_ROOT_PATH="/opt/airflow/git/dbt-azure-monitor.git/dags/dbt/dbtAzureMonitor" # --> managed airflow path
 #PROJECT_ROOT_PATH="/home/gopal/dbt-workspace/jaffle_shop/dags/dbt/jaffle_shop"  --> local development path
@@ -24,17 +25,32 @@ profile_config = ProfileConfig(
 )
 
 with DAG(
-        dag_id="dbtAzureMonitor",
+        dag_id="airlinedemo_dag",
         start_date=datetime(2024, 2, 15),
-        schedule="@daily",
-):
+        schedule_interval="@daily",
+) as dag:
     e1 = EmptyOperator(task_id="pre_dbt")
 
     dbt_tg = DbtTaskGroup(
-        project_config=ProjectConfig(dbt_project_path=PROJECT_ROOT_PATH),
+        task_id="dbt_tasks",
+        project_config=ProjectConfig(
+            dbt_project_path=PROJECT_ROOT_PATH,
+            manifest_path=f"{PROJECT_ROOT_PATH}/target/manifest.json",
+        ),
         profile_config=profile_config,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_MANIFEST,
+            # By default cosmos generate dag that execute model and test for that model, if you don't want to use test then pass test_behavior=TestBehavior.NONE
+            # test_behavior=TestBehavior.NONE
+        ),
     )
 
     e2 = EmptyOperator(task_id="post_dbt")
 
-    e1 >> dbt_tg >> e2
+    run_this_2 = BashOperator(
+        task_id="run_after_loop",
+        bash_command="dbtlog --env=databricks --dwhcid=databricks_connection --azmonitorcid=azure_monitor",
+        trigger_rule="all_done"
+    )
+
+    e1 >> dbt_tg >> e2 >> run_this_2
